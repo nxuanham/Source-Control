e8e2f48a07432e189c0d3bf5c62310d82a86dbe8
Nguyen Xuan Ham <nxuanham@gmail.com>
2024-04-16 12:03:24 +0700

    Initial Commit

---
+++ Source Control/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Source Control
+//
+//  Created by Xuan Ham Nguyen on 16/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Source Control/Source_ControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  Source_ControlApp.swift
+//  Source Control
+//
+//  Created by Xuan Ham Nguyen on 16/4/24.
+//
+
+import SwiftUI
+
+@main
+struct Source_ControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
