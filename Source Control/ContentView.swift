885b4fd0bfa9065f717ccc731ef8a54e5cf8678a
Nguyen Xuan Ham <nxuanham@gmail.com>
2024-04-16 12:09:53 +0700

    Add navigation

--- Source Control/ContentView.swift
+++ Source Control/ContentView.swift
@@ -10,12 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            NavigationView {
+                Rectangle().fill(Color.green)
+                    .ignoresSafeArea(.all)
+                    .navigationTitle("Source Control")
+            }
         }
-        .padding()
     }
 }
 
